fc04faa263892bf4cf457f81add3e7126c3ee61c
Ángel Fernández Barrios <angel.fernandez91@gmail.com>
2023-02-15 17:56:59 +0100

    Initial Commit

---
+++ TrantorLibrary/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrantorLibrary
+//
+//  Created by Angel Fernandez Barrios on 15/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrantorLibrary/TrantorLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrantorLibraryApp.swift
+//  TrantorLibrary
+//
+//  Created by Angel Fernandez Barrios on 15/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TrantorLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
